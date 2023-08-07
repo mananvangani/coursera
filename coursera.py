e490f2d491462dfaa587544f31299a776b4f06f4
mananvangani <141165465+mananvangani@users.noreply.github.com>
2023-08-07 06:45:07 +0530

    Add files via upload

---
+++ coursera.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #The title for this notebook is coursera 
+
+# In this notebook i will be constructing my final project for course 2 of my specialization
+# It has 13 exercises in total
+# 
+
+# Exercise 1 
+# 
+# Python: Python is one of the most popular and versatile programming languages for data science. It has a rich ecosystem of libraries and frameworks, such as Pandas, NumPy, SciPy, Scikit-learn, TensorFlow, and PyTorch, which make it an excellent choice for data manipulation, analysis, and machine learning.
+# 
+# R: R is specifically designed for statistical computing and data analysis. It provides a wide range of packages for data visualization, statistical modeling, and data manipulation, making it a favorite among statisticians and data scientists.
+# 
+# SQL: SQL (Structured Query Language) is essential for working with databases. Data scientists often use SQL to retrieve and manipulate data stored in relational databases.
+# 
+# Julia: Julia is a high-level, high-performance programming language for technical computing. It is gaining popularity in the data science community due to its speed and ease of use for numerical and scientific computing.
+
+# Exercise 2
+# For Python:
+# 
+# NumPy: A fundamental library for numerical computing in Python, providing support for large, multi-dimensional arrays and matrices.
+# 
+# Pandas: A powerful library for data manipulation and analysis, offering data structures like DataFrame to handle and analyze structured data efficiently.
+# 
+# Matplotlib: A versatile plotting library for creating static, interactive, and animated visualizations in Python.
+# 
+# Seaborn: Built on top of Matplotlib, Seaborn provides a high-level interface for creating attractive statistical graphics.
+# 
+# Scikit-learn: A comprehensive library for machine learning, offering tools for classification, regression, clustering, dimensionality reduction, and more.
+# 
+# TensorFlow: Developed by Google, TensorFlow is an open-source deep learning library that allows building and training complex neural networks.
+# 
+# Keras: A high-level neural networks API that runs on top of TensorFlow, designed to make deep learning implementation more straightforward.
+# 
+# PyTorch: An open-source machine learning library developed by Facebook's AI Research lab, known for its flexibility and dynamic computation capabilities.
+# 
+# Statsmodels: A library that provides classes and functions for statistical models and hypothesis testing in Python.
+# 
+# For R:
+# 
+# ggplot2: A popular data visualization library in R based on the grammar of graphics.
+# 
+# dplyr: A powerful package for data manipulation, providing a concise syntax for filtering, arranging, summarizing, and transforming data.
+# 
+# tidyr: Works seamlessly with dplyr, offering functions for data reshaping and tidying.
+# 
+# caret: A comprehensive package for building machine learning models and performing model selection and evaluation.
+# 
+# lubridate: A package for handling date and time data in R.
+# 
+# randomForest: Implements random forest, a popular ensemble learning method, for classification and regression tasks.
+# 
+# glmnet: Provides functions for fitting regularized linear models using the Lasso or Elastic Net methods.
+# 
+# forecast: Focused on time series forecasting, offering various models and tools for time series analysis.
+
+# list of data science tools
+# 
+# Python (with Jupyter Notebook)	A versatile programming language with an extensive ecosystem of data science libraries (NumPy, Pandas, Matplotlib, Scikit-learn, etc.) and Jupyter Notebook for interactive data analysis and visualization.
+# 
+# 
+# R (with RStudio)	A language specifically designed for statistical computing, widely used for data analysis and visualization, and RStudio as a powerful integrated development environment (IDE) for R.
+# 
+# 
+# 
+# SQL (Structured Query Language)	Essential for working with relational databases and conducting database queries for data extraction and manipulation.
+# 
+# 
+# 
+# Excel	Widely used for data analysis, reporting, and visualization. Excel supports various statistical and data analysis functions.
+# 
+# 
+# Tableau	A powerful data visualization tool that allows users to create interactive and dynamic visualizations from different data sources.
+# 
+# 
+# Power BI	Microsoft's business analytics service that provides interactive visualizations and business intelligence capabilities.
+# 
+# 
+# Apache Spark	A fast and distributed data processing engine that supports big data analytics and machine learning.
+# 
+# 
+# TensorFlow	An open-source deep learning library developed by Google for building and training complex neural networks.
+# 
+# 
+# PyTorch	An open-source machine learning library developed by Facebook's AI Research lab, known for its flexibility and dynamic computation capabilities.
+
+# exercise 7
+# 
+# 
+# Arithmetic expressions involve basic mathematical operations such as addition, subtraction, multiplication, division, and exponentiation. The order of operations (PEMDAS/BODMAS) determines the sequence in which the operations are evaluated
+# Addition:
+# Subtraction:
+# Multiplication:
+# Division:
+# Exponentiation:
+
+# In[1]:
+
+
+x, y = 10, 20;
+z = x+y;
+k=x*y;
+print('The sum of', x ,'and', y ,'is :', z); 
+print('the multiplication of',x, 'and',y,'is',k)
+
+
+# In[2]:
+
+
+def minutes_to_hours(minutes):
+    hours = minutes / 60
+    return hours
+
+
+# exercise 10
+# objectives
+# Create a Jupyter Notebook with markdown and code cells
+# List examples of languages, libraries and tools used in Data Science
+# Share your Jupyter Notebook publicly on GitHub
+# Evaluate notebooks submitted by your peers using the provided rubric
+# Demonstrate proficiency in Data Science toolkit knowledge
+
+# exercise 11
+# Manan Vangani
+
+# In[ ]:
+
+
+
+
